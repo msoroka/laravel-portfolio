bb0e0195e90413bc3ade129a508067236664c777
Mateusz Soroka <xmsoroka@gmail.com>
2019-04-05 21:56:17 +0200

    Authentication made

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('profile', function () {
+    return 'It works';
+})->middleware('verified');
