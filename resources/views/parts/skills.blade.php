11a8bbdf4c0e7eb07e4e102e458036aa062c413a
Mateusz Soroka <xmsoroka@gmail.com>
2019-04-14 00:21:32 +0200

    Skills view module

--- resources/views/parts/skills.blade.php
+++ resources/views/parts/skills.blade.php
@@ -1,3 +1,14 @@
-<div class="skills" id="skills">
+<div class="skills row" id="skills">
     <div class="separator"><h3>Skills</h3></div>
+    @foreach($skills as $skill)
+        <div class="skill col-lg-3 col-md-6 col-sm-12" style="background: {{ $skill->color }} !important;">
+            <img src="{{ asset('images/' . $skill->image) }}"
+                 alt="{{ $skill->name }}">
+            <div class="skill-desc">
+                <h3><strong>{{ $skill->name }}</strong></h3>
+{{--                <p>{{ $skill->description }}</p>--}}
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex laudantium qui quos. Dolores esse modi, odit quisquam quo saepe velit?</p>
+            </div>
+        </div>
+    @endforeach
 </div>
